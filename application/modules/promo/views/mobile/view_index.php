03e60f9eeba0d858db3843bcbd73a9c1fbba4d3d
Ramdhan <mramdhanass@gmail.com>
2026-05-04 14:12:23 +0700

    fix layout promo

--- application/modules/promo/views/mobile/view_index.php
+++ application/modules/promo/views/mobile/view_index.php
@@ -1,23 +1,23 @@
-<div class="promo_page m-t-1">
+<div class="promo_page mt-3">
 	<!-- <div class="col-md-3">
 			<?php //$this->load->view("c/_form-filter-search") 
 			?>
 		</div> -->
 	<?php $imgonmobile = ($this->agent->is_mobile()) ? base_url() . 'timthumb?h=200&src=' : ''; ?>
 	<?php foreach ($listpromo as $i => $key) : ?>
-		<div class="card p-b-1 p-x-1">
-			<div class="row">
-				<div class="col-xs-12 p-a-0">
+		<div class="card pb-2 px-2 mb-3">
+			<div class="row g-0">
+				<div class="col-12 p-0">
 					<a href="<?php echo base_url() ?>promo/<?php echo $key["url"] ?>">
 						<img title="<?php echo $key["name"] ?>" src="<?php echo $imgonmobile ?><?php echo base_url() ?>public/image/promo/<?php echo $key["img"] ?>" class="img-fluid" alt="<?php echo $key["name"] ?>" style="width: 100%; max-height:150px; border-bottom:0.5px solid #eee;">
 					</a>
 				</div>
-				<div class="clearfix m-b-1"></div>
-				<div class="col-xs-12">
+				<div class="clearfix mb-3"></div>
+				<div class="col-12">
 					<?php $str = str_split($key["description"], 150); ?>
-					<h5 class="fbold f15"><?php echo $key["name"] ?></h5>
-					<h6 class="text-muted f11"><?php echo count($str) > 1 ? $str[0] . "..." : $str[0]; ?></h6>
-					<a href="<?php echo base_url() ?>promo/<?php echo $key["url"] ?>" class="forange f11">Lihat Selengkapnya</a>
+					<h5 class="fw-bold mb-2" style="font-size: 0.9rem;"><?php echo $key["name"] ?></h5>
+					<p class="text-secondary small mb-2"><?php echo count($str) > 1 ? $str[0] . "..." : $str[0]; ?></p>
+					<a href="<?php echo base_url() ?>promo/<?php echo $key["url"] ?>" class="text-decoration-none small text-warning">Lihat Selengkapnya</a>
 				</div>
 			</div>
 		</div>
