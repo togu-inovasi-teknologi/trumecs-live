09f388d27025f7632ddea3a5ab9c0340a5272500
Ramdhan <mramdhanass@gmail.com>
2026-05-07 15:34:48 +0700

    tombol beli

--- application/modules/product/views/_item_product_c.php
+++ application/modules/product/views/_item_product_c.php
@@ -77,15 +77,16 @@
                     </div>
                 </div>
             </div>
-        </a>
-
-        <!-- Tombol Beli - Selalu di bawah -->
-        <div class="card-footer border-0 p-0 mt-auto">
+            <div class="card-footer border-0 p-0 mt-auto">
             <button id="btnbuy<?php echo $key['id'] ?>"
                 class="btn btn-primary w-100 rounded-0 py-2">
                 <i class="bi bi-cart3 me-1"></i>
                 Beli
             </button>
         </div>
+        </a>
+
+        <!-- Tombol Beli - Selalu di bawah -->
+        
     </div>
 </div>
