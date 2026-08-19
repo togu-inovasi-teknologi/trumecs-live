e8c8ba7cd5c3598dcde7f3c1c9d2481c734cb9c3
Ramdhan <mramdhanass@gmail.com>
2026-08-19 14:08:42 +0700

    fix

--- application/modules/backendproduct/controllers/Backendproduct.php
+++ application/modules/backendproduct/controllers/Backendproduct.php
@@ -90,8 +90,6 @@ class Backendproduct extends MX_Controller
     {
 
         $result = $this->spreadsheetapi->syncProductsFromSheetToDB('Product-list');
-        var_dump($result);
-        die;
 
         if ($result['success']) {
             $this->session->set_flashdata(
